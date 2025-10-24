cc8a0a57f338d354fff005962c7fc6a97ef489fc
Andre <andre.ramos15@fatec.sp.gov.br>
2025-10-24 20:39:44 -0300

    adicionando arquivos iniciais

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import cors from 'cors'
+import exampleRoutes from './routes/Example.routes'
+import requestLogger from './middlewares/requestLogger.middleware'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(requestLogger)
+
+app.use('/', exampleRoutes)
+
+export default app
